b8ea66233172e22dcd3ecd9b83b530c813754b66
Simone Romano <sim.romano@gmail.com>
2024-04-28 18:50:58 +0200

    with modules quick & dirty

--- js/Apple.js
+++ js/Apple.js
@@ -1,17 +1,14 @@
-class Apple{
-    constructor(
-        x = Math.floor(Math.random() * grid.size.width),
-        y = Math.floor(Math.random() * grid.size.height),
-        color = '#ff3333'
-    ){
-        this.x = x;
-        this.y = y;
-        this.color = color;
-    }
+export default class Apple {
+  constructor(grid, x = Math.floor(Math.random() * grid.size.width), y = Math.floor(Math.random() * grid.size.height), color = '#ff3333') {
+    this.grid = grid;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
 
-    draw(){
-        noStroke();
-        fill(this.color);
-        rect(width / grid.size.width * this.x, height / grid.size.height * this.y, width / grid.size.width, height / grid.size.height);
-    }
+  draw() {
+    noStroke();
+    fill(this.color);
+    rect((width / this.grid.size.width) * this.x, (height / this.grid.size.height) * this.y, width / this.grid.size.width, height / this.grid.size.height);
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+import Snake from './Snake.js';
+import Apple from './Apple.js';
+
 const grid = {};
 
 let snake;
@@ -7,66 +10,67 @@ let scoreOutput;
 let score = 0;
 let fps = 4;
 
-function setup(){
-    createCanvas(800, 800);
-    background(80, 80, 80);
+function setup() {
+  createCanvas(800, 800);
+  background(80, 80, 80);
+  frameRate(fps);
+
+  grid.color = '#444';
+  grid.weight = 2;
+  grid.size = {
+    width: 21,
+    height: 21,
+  };
+
+  grid.size.pixelX = width / grid.size.width;
+  grid.size.pixelY = height / grid.size.height;
+
+  snake = new Snake(grid);
+
+  apples.push(new Apple(grid));
+  apples.push(new Apple(grid));
+
+  scoreOutput = document.createElement('span');
+  scoreOutput.id = 'score';
+  scoreOutput.textContent = score = 0;
+  document.body.prepend(scoreOutput);
+}
+
+function draw() {
+  background(80, 80, 80);
+  drawGrid();
+
+  apples.forEach((apple) => apple.draw());
+  snake.draw();
+
+  const collidingApple = apples.find((apple) => apple.x === snake.x && apple.y === snake.y);
+  if (collidingApple) {
+    // Eating apple
+    apples.splice(apples.indexOf(collidingApple), 1);
+    snake.append();
+    apples.push(new Apple(grid));
+    scoreOutput.textContent = ++score;
+    fps += 1;
     frameRate(fps);
+  }
 
-    grid.color = '#444';
-    grid.weight = 2;
-    grid.size = {
-        width: 21,
-        height: 21
-    }
-
-    grid.size.pixelX = width / grid.size.width;
-    grid.size.pixelY = height / grid.size.height;
-
-    snake = new Snake();
-
-    apples.push(new Apple());
-    apples.push(new Apple());
-
-    scoreOutput = document.createElement('span');
-    scoreOutput.id = 'score';
-    scoreOutput.textContent = (score = 0);
-    document.body.prepend(scoreOutput);
+  if (snake.body.slice(1).find((i) => i.x === snake.x && i.y === snake.y)) {
+    snake.die();
+    document.getElementById('game-over').classList.remove('hidden');
+  }
 }
 
-function draw(){
-    background(80, 80, 80);
-    drawGrid();
-    
-    apples.forEach(apple => apple.draw());
-    snake.draw();
-
-    const collidingApple = apples.find(apple => apple.x === snake.x && apple.y === snake.y);
-    if(collidingApple){
-        // Eating apple
-        apples.splice(apples.indexOf(collidingApple), 1);
-        snake.append();
-        apples.push(new Apple());
-        scoreOutput.textContent = ++score;
-        fps += 1;
-        frameRate(fps);
-    }
-
-    if(snake.body.slice(1).find(i => i.x === snake.x && i.y === snake.y)){
-        snake.die();
-        document.getElementById('game-over').classList.remove('hidden');
-    }
+function drawGrid() {
+  stroke(grid.color);
+  noFill();
+  strokeWeight(grid.weight);
+  for (let x = 0; x < grid.size.width; x += 1) for (let y = 0; y < grid.size.height; y += 1) rect(grid.size.pixelX * x, grid.size.pixelY * y, grid.size.pixelX, grid.size.pixelY);
 }
 
-function drawGrid(){
-    stroke(grid.color);
-    noFill();
-    strokeWeight(grid.weight);
-    for(let x = 0; x < grid.size.width; x += 1)
-        for(let y = 0; y < grid.size.height; y += 1)
-            rect( grid.size.pixelX * x, grid.size.pixelY * y, grid.size.pixelX, grid.size.pixelY);
+function keyPressed() {
+  if (key.startsWith('Arrow')) snake.direction = key.substring(5).toLowerCase();
 }
 
-function keyPressed(){
-    if(key.startsWith('Arrow'))
-        snake.direction = key.substring(5).toLowerCase();
-}
+window.setup = setup;
+window.draw = draw;
+window.keyPressed = keyPressed;
